a7c8f8c643333685e15aad9369024238a4e7ed82
Malcolm Herring <malcolm.herring@btinternet.com>
2020-02-01 14:57:43 +0000

    first test done

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 swig -java -I../Hamlib/include/ -outcurrentdir -package hamlib -outdir src/hamlib -o jni/hamlib_wrap.c ../Hamlib/bindings/hamlib.swg
-gcc -fPIC -c -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include/darwin -I../Hamlib/src -o jni/libhamlib_wrap.dylib jni/hamlib_wrap.c
+gcc -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include/darwin -O0 -g3 -Wall -c -o "jni/hamlib_wrap.o" "jni/hamlib_wrap.c"
+gcc -c -o jni/main.o main.c
+gcc -fPIC -o jni/libhamlib_wrap.dylib jni/hamlib_wrap.o jni/main.o -lhamlib
+rm jni/*.o
+sudo cp jni/libhamlib_wrap.dylib /usr/local/lib/
+
---
+++ src/test/Test.java
@@ -0,0 +1,29 @@
+package test;
+
+import hamlib.Hamlib;
+import hamlib.powerstat_t;
+import hamlib.rig_debug_level_e;
+
+public class Test {
+    
+    static { System.loadLibrary("hamlib_wrap"); }
+
+    public static void main(String args[]) throws Exception {
+        
+        Hamlib.rig_set_debug(rig_debug_level_e.RIG_DEBUG_NONE);
+        hamlib.Rig rig = new hamlib.Rig(Integer.parseUnsignedInt(args[0]));
+        rig.getState().getRigport().setPathname(args[1]);
+        rig.open();
+        System.out.println("Rig started");
+        Thread.sleep(3000);
+        double freq = rig.get_freq(hamlib.HamlibConstants.RIG_VFO_MAIN);
+        System.out.println(freq);
+        Thread.sleep(3000);
+        rig.set_freq(hamlib.HamlibConstants.RIG_VFO_MAIN, freq + 1.0e6);
+        System.out.println(rig.get_freq(hamlib.HamlibConstants.RIG_VFO_MAIN));
+        Thread.sleep(3000);
+        rig.set_powerstat(powerstat_t.RIG_POWER_OFF);
+        rig.close();
+        System.out.println("Rig stopped");
+    }
+}
