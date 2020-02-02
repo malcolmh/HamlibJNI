13919c2a1b6cb468adf01f5f3e341c098253fa97
Malcolm Herring <malcolm.herring@btinternet.com>
2020-02-02 04:58:49 +0000

    add java build

--- build_macos.sh
+++ build_macos.sh
@@ -1,7 +1,5 @@
 swig -java -I../Hamlib/include/ -outcurrentdir -package hamlib -outdir src/hamlib -o jni/hamlib_wrap.c ../Hamlib/bindings/hamlib.swg
-gcc -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include/darwin -O0 -g3 -Wall -c -o "jni/hamlib_wrap.o" "jni/hamlib_wrap.c"
-gcc -c -o jni/main.o main.c
-gcc -fPIC -o jni/libhamlib_wrap.dylib jni/hamlib_wrap.o jni/main.o -lhamlib
-rm jni/*.o
+gcc -fPIC -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/jdk1.8.0_241.jdk/Contents/Home/include/darwin -O0 -g3 -Wall -c -o "jni/hamlib_wrap.o" "jni/hamlib_wrap.c"
+gcc -shared -o jni/libhamlib_wrap.dylib jni/hamlib_wrap.o -lhamlib
 sudo cp jni/libhamlib_wrap.dylib /usr/local/lib/
 
